04f423b971d2f4a76c4bbcb3be9e1fe7b688c17a
Tayyaba Rashid <165508609+TayyabaRashid123@users.noreply.github.com>
2024-03-31 23:51:22 +0500

    Add files via upload

---
+++ ASSIGNMENT14.js
@@ -0,0 +1,2 @@
+var guestList = ["SHAZIA", "SAMINA", "RIZWANA"];
+guestList.forEach(function (oneGuest) { return console.log("SALAM ".concat(oneGuest, ",Would you like to dinner with me?")); });
---
+++ ASSIGNMENT14.ts
@@ -0,0 +1,2 @@
+let guestList= ["SHAZIA","SAMINA","RIZWANA"];
+guestList.forEach (oneGuest => console.log(`SALAM ${oneGuest},Would you like to dinner with me?`));
