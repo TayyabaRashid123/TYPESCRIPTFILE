43d23132cc831eac9cd5afebeedc299b8eff39f6
Tayyaba Rashid <165508609+TayyabaRashid123@users.noreply.github.com>
2024-03-31 23:39:20 +0500

    Add files via upload

---
+++ ASSIGNMENT13.js
@@ -0,0 +1,2 @@
+var transportationModes = ["SPORT CAR", "BUS", "SPORT BIKE", "CAR"];
+transportationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ ASSIGNMENT13.ts
@@ -0,0 +1,2 @@
+let transportationModes = ["SPORT CAR","BUS","SPORT BIKE","CAR"];
+transportationModes.forEach(mode=>console.log(`I would like to own a ${mode}`));
