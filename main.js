970704296d772b7bdc5421446c83ef16680342ce
Tayyaba Rashid <165508609+TayyabaRashid123@users.noreply.github.com>
2024-04-08 01:42:25 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
-//creating a guest list array
-var guestList = ["SHAZIA", "SAMINA", "RIZWANA"];
-// 1 guest is not coming
-var dontCome = guestList[2];
-//message for not coming
-console.log(dontCome, "she is not coming");
-//add 1 new guest
-guestList.splice(2, 1, "SAIMA");
-// asking new guest that would you like to dinner with me
-guestList.forEach(function (guest) { return console.log("hello ".concat(guest, ",would you like to dinner with me?;")); });
-// meassage for bigger table
-console.log("GOOD NEWS ! WE HAVE FOUND A BIGGER DINNER TABLE FOR DINNING");
-// adding a new guest at starting index of array
-guestList.unshift("RASHID");
-// adding a new guest at ending index of array
-guestList.push("TOOBA");
-//get a middle index of guest list array
-var middleIndex = Math.floor(guestList.length / 2);
-//adding a new guest as middle of an array
-guestList.splice(middleIndex, 0, "HAMZA");
-//print message
-console.log("updated list of oue guest");
-//invitation message one by one
-guestList.forEach(function (oneGuest) { return console.log("SALAM ".concat(oneGuest, "would you like to dinner with me)?")); });
-//inform that only 2 guests are invited for dinner
-console.log("Unfortunately, The new dinner table won't arrive on time, As I can only invite 2 guests dinner with me");
-//using while loop to remove the guest from the array until only two guest are invited
-while (guestList.length > 2) {
-    var removedguest = guestList.pop();
-    console.log("Sorry,".concat(removedguest, " i cant invite you to dinner "));
-}
-// print a message last two guest are invited
-console.log("Invitation to the last two Guests");
-guestList.forEach(function (lasttwo) { return console.log("luckily ".concat(lasttwo, ",you are still invited to dinner")); });
-//removing now both of lasttwo guest from lisy
-guestList.pop();
-guestList.pop();
-console.log("Empty list:", guestList);
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+//making an array in original order
+var countriestovisit = ["Jeddah", "China", "Demark", "Brazil"];
+console.log("Orginal order:", countriestovisit);
+//print an array with alphabetical order without modifying actual one
+console.log("Alphabetical Order:", __spreadArray([], countriestovisit, true).sort());
+//ensure that array is in original order
+console.log('still in orginal order', countriestovisit);
+//print a array in reverse order without modifying actual order
+console.log.apply(console, __spreadArray(["Reverse Order:"], countriestovisit.reverse(), false));
+//ensure that array is in original order
+console.log('still in orginal order', countriestovisit);
+//we have change the orignal array order now
+console.log("Original array Reserved:", countriestovisit.reverse());
+//print the array to get back to orginal order
+console.log("back to get original order ", countriestovisit.reverse());
+//print the array to show that its order has been changed in alphabetical order
+console.log("Sorted in Alphabetical order:", countriestovisit.sort());
+///we have change again the orignal array order again
+console.log("Original array Reserved Again:", countriestovisit.reverse());
--- main.ts
+++ main.ts
@@ -1,61 +1,31 @@
-//creating a guest list array
-let guestList=["SHAZIA","SAMINA","RIZWANA"];
+//making an array in original order
+let countriestovisit : string[] = ["Jeddah","China" , "Demark" , "Brazil"]
+console.log("Orginal order:",countriestovisit);
 
-// 1 guest is not coming
-let dontCome = guestList [2];
-
-//message for not coming
-console.log(dontCome,"she is not coming");
-
-//add 1 new guest
-guestList.splice(2,1, "SAIMA");
-
-// asking new guest that would you like to dinner with me
-guestList.forEach (guest => console.log(`hello ${guest},would you like to dinner with me?;`));
-
-// meassage for bigger table
-console.log("GOOD NEWS ! WE HAVE FOUND A BIGGER DINNER TABLE FOR DINNING");
-
-// adding a new guest at starting index of array
-guestList.unshift("RASHID");
-
-// adding a new guest at ending index of array
-guestList.push("TOOBA");
-
-//get a middle index of guest list array
-let middleIndex : number = Math.floor(guestList.length / 2); 
-
-//adding a new guest as middle of an array
-guestList.splice (middleIndex,0 ,"HAMZA");
-
-//print message
-console.log("updated list of oue guest");
-//invitation message one by one
-guestList.forEach(oneGuest=>console.log(`SALAM ${oneGuest}would you like to dinner with me)?`));
+//print an array with alphabetical order without modifying actual one
+console.log("Alphabetical Order:",[...countriestovisit].sort());
 
 
-//inform that only 2 guests are invited for dinner
-console.log("Unfortunately, The new dinner table won't arrive on time, As I can only invite 2 guests dinner with me");
+//ensure that array is in original order
+console.log('still in orginal order',countriestovisit);
+
+//print a array in reverse order without modifying actual order
+console.log("Reverse Order:",...countriestovisit.reverse());
 
 
-//using while loop to remove the guest from the array until only two guest are invited
-while(guestList.length > 2)
-{
-   let removedguest = guestList.pop();
-   console.log(`Sorry,${removedguest} i cant invite you to dinner `)
-}
-
-// print a message last two guest are invited
-console.log("Invitation to the last two Guests");
-guestList.forEach(lasttwo => console.log(`luckily ${lasttwo},you are still invited to dinner`));
+//ensure that array is in original order
+console.log('still in orginal order',countriestovisit);
 
 
-//removing now both of lasttwo guest from lisy
-guestList.pop();
-guestList.pop();
-console.log("Empty list:",guestList);
+//we have change the orignal array order now
+console.log("Original array Reserved:",countriestovisit.reverse());
 
 
+//print the array to get back to orginal order
+console.log("back to get original order ",countriestovisit.reverse());
 
+//print the array to show that its order has been changed in alphabetical order
+console.log("Sorted in Alphabetical order:",countriestovisit.sort());
 
-
+///we have change again the orignal array order again
+console.log("Original array Reserved Again:",countriestovisit.reverse());
